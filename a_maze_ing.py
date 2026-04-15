a8bd75ca783ae89792563d9f799c053eb6e0c809
vsudak <vsudak@student.codam.nl>
2026-04-15 12:24:13 +0200

    working on the path in output file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -15,26 +15,83 @@ def decode(cell: Cell) -> str:
     east = int(cell.e)
     south = int(cell.s)
     west = int(cell.w)
+    # print(west)
     result = str(west) + str(south) + str(east) + str(north)
+    # print(result)
     return result
 
 
-def write_into_file(grid: list[list[Cell]], output_file) -> None:
+def get_right_dir(cell: Cell, maze: Maze) -> dict[str, Cell]:
+    x, y = cell.position
+    result = ()
+    # checing from 4 sides
+    if x - 1 >= 0:
+        if maze.grid[y][x - 1].special == " P":
+            if maze.grid[y][x - 1].e is False and cell.w is False:
+                result = ("W", maze.grid[y][x - 1])
+    if x + 1 < maze.width:
+        if maze.grid[y][x + 1].special == " P":
+            if maze.grid[y][x + 1].w is False and cell.e is False:
+                result = ("E", maze.grid[y][x + 1])
+    if y - 1 >= 0:
+        if maze.grid[y - 1][x].special == " P":
+            if maze.grid[y - 1][x].n is False and cell.s is False:
+                result = ("N", maze.grid[y - 1][x])
+    if y + 1 < maze.height:
+        if maze.grid[y + 1][x].special == " P":
+            if maze.grid[y + 1][x].s is False and cell.n is False:
+                result = ("S", maze.grid[y + 1][x])
+    # print(result, cell.position)
+    return result
+
+
+def get_directions(maze: Maze) -> str:
+    current = maze.grid[maze.entry[1]][maze.entry[0]]
     result = ""
-    for row in grid:
+    next_cell: Cell = None
+    print("get dir")
+    while next_cell.position != maze.exit:
+        dir, next_cell = get_right_dir(current, maze)
+        # print(right_dir)
+        # for d, cell in right_dir.items():
+        #     dir = d
+        #     next_cell = cell
+        # dir = right_dir.keys()
+        # print(dir)
+        # dir, next_cell = get_right_dir(current, maze).items()
+        result += dir
+        current = next_cell
+        if next_cell.special == " E":
+            break
+    return result
+
+
+def write_into_file(maze: Maze) -> None:
+    result = ""
+    for row in maze.grid:
         for cell in row:
             string = decode(cell)
-            result += str(int(string, base=16))
+
+            to_add = str(hex(int(string, base=2)))
+
+            result += to_add.removeprefix("0x").capitalize()
         result += "\n"
+    result += "\n"
+    entry = str(maze.entry).removeprefix("(")
+    result += entry.removesuffix(")") + "\n"
+    finish = str(maze.exit).removeprefix("(")
+    print(finish)
+    result += finish.removesuffix(")") + "\n"
+    result += get_directions(maze)
     try:
-        with open("output_file", mode="w") as f:
+        with open(maze.output_file, mode="w") as f:
             f.write(result)
     except Exception as e:
         raise e
 
 
 def run_menu(my_maze: Maze) -> None:
-    show_path = False 
+    show_path = False
 
     colors = ["\033[0m", "\033[31m", "\033[32m", "\033[33m", "\033[34m"]
     #          default       red          green       yellow      blue
@@ -43,11 +100,10 @@ def run_menu(my_maze: Maze) -> None:
     while True:
         """Escape sequence to clean terminal screen"""
         print("\033[H\033[J", end="")
-        system("virt_env/bin/bash clear")
+        # system("clear")
         my_maze.print_grid(show_path, colors[color_index])
-        # my_maze.print_grid()
 
-        print("\n=== A-Maze-ing===")
+        print("\n=== A-Maze-ing ===")
         print("1. Regenerate maze")
         print("2. Show/Hide path")
         print("3. Rotate colors")
@@ -62,23 +118,30 @@ def run_menu(my_maze: Maze) -> None:
             my_maze.insert_forty2(my_maze.ft())
             my_maze.path_gen()
         elif choice == "2":
-            show_path = not show_path
+            if show_path is False:
+                show_path = True
+            else:
+                show_path = False
+            # show_path = not show_path
             print("In progress...")
         elif choice == "3":
             color_index = (color_index + 1) % len(colors)
-        elif choice == "4":
+        elif choice == "4" or choice == "q":
             break
 
 
-def print_grid_of_pos(grid: list[list[Cell]]) -> None:
-    for row in grid:
+def print_grid_of_path(maze: list[list[Cell]]):
+    for row in maze:
         for cell in row:
-            print(cell.position, end="")
+            if cell.path is True:
+                print("1", end="")
+            else:
+                print("0", end="")
         print()
-
 
 def main() -> None:
     if len(sys.argv) == 2:
+        # it can be any file, maybe that ends up on .txt
         if sys.argv[1] == "config.txt":
             with open(sys.argv[1], "r") as config_file:
                 config_data = config_file.read()
@@ -93,15 +156,14 @@ def main() -> None:
             raise Exception("We are expecting config.txt as an argument")
         try:
             my_maze = Maze(**data_4_maze)
-            cols = my_maze.width * 6
-            lines = my_maze.height
-            system(f"mode con: cols={cols} lines={lines}")
             my_maze.create_grid()
             my_maze.insert_forty2(my_maze.ft())
             my_maze.path_gen()
-            write_into_file(my_maze.grid, my_maze.output_file)
+            print_grid_of_path(my_maze.grid)
+            # print_grid(my_maze.grid)
+            # write_into_file(my_maze)
             my_maze.print_grid()
-            run_menu(my_maze)
+            # run_menu(my_maze)
 
         except Exception as e:
             print(str(e))
